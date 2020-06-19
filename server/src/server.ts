1113b2ed981cd97cb5ff9f2757a0a1e6451a4d5f
hjunor <hjunor@gmail.com>
2020-06-18 23:36:57 -0300

    index of the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
+import routes from './routes';
+
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.get('/users', (req, res) => {
-  console.log('listagem de usuario');
-  res.json({
-    name: 'Heberth',
-    lastName: 'junor',
-  });
-});
-
-app.listen(3000);
+app.listen(3333);
+//time of the aplication 1:23:03, save on git start
